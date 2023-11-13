93b3c31d5010cd7130f10aff98c9aa8945aadbd1
malken21 <Malken_Marumasa@outlook.jp>
2023-11-13 20:58:01 +0900

    Update upload.ps1

--- upload.ps1
+++ upload.ps1
@@ -47,7 +47,8 @@ function  upload($URL, $filePath, $try_count = 0) {
         else {
             Write-Host "Discordにファイルを送信できませんでした"
             Start-Sleep $cooldown # クールダウン
-            if ( $try_count -lt $maxAttempts) { upload $URL $filePath $try_count+1 }
+            $try_count++
+            if ( $try_count -lt $maxAttempts) { upload $URL $filePath $try_count }
         }
     }
     catch {
